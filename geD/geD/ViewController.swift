bf28b79756b2809041df86d5b44bad32114ae3a6
butga@naver.com <butga@naver.com>
2021-05-17 17:48:33 +0900

    first commit

---
+++ geD/geD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  geD
+//
+//  Created by 김민순 on 2021/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
